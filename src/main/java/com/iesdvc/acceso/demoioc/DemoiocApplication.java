5a01e7a59aa70b39387ac4f40489122233669f17
lucopa <lcobpar754@g.educaand.com>
2024-03-06 18:41:25 +0100

    Proyecto

---
+++ src/main/java/com/iesdvc/acceso/demoioc/DemoiocApplication.java
@@ -0,0 +1,13 @@
+package com.iesdvc.acceso.demoioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoiocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoiocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iesdvc/acceso/demoioc/DemoiocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesdvc.acceso.demoioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoiocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
